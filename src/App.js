082b8a276d424249f183e7f79cb84a602a49609a
Arditti93 <alex.arditti@hotmail.com>
2022-11-01 17:27:11 +0000

    added read operation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
+import {useState} from 'react'
 import Register from "./components/Register";
+import ReadUsers from "./components/ReadUsers";
 
 
 
 const App = () => {
+  const [user, setUser] = useState()
+
   return (
     <div>
       <h1>Register</h1> 
-      <Register/>
+      <Register setter={setUser}/>
+      {user ?
+      <div>
+        <h2> Hello! welcome {user}</h2>
+        <ReadUsers />
+      </div>
+        : 
+        <h2>Please Register</h2>
+      }
     </div>
   );
 };
--- src/utils/index.js
+++ src/utils/index.js
@@ -11,8 +11,26 @@ export const regUser = async (username, email, password, setter) => {
         })
         const data = await response.json()
         console.log(data)
-
+        setter(data.username)
     } catch (error) {
         console.log(error)
     }
 }
+
+export const readUsers = async () => {
+    try {
+        const response = await fetch("http://localhost:5001/readUser", {
+            method: "GET",
+            headers: {"Content-Type": "application/json"}
+        })
+        const data = await response.json()
+        //console.log(data.user)
+        const usernames = data.user.map(users => users.username)
+        //console.log(usernames)
+        return usernames
+    } catch (error) {
+        console.log(error)
+    }
+}
+//TODO: add delete and update fetch requests
+
